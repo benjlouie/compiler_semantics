d2ad855fc532343c669ee9f52f855fa755c5c6fa
mkarasz@cs.nmt.edu <mkarasz@cs.nmt.edu>
2016-10-29 16:45:14 -0600

    More skeleton

--- symbolTable.h
+++ symbolTable.h
@@ -1,6 +1,9 @@
+#pragma once
+#include <unordered_map>
+
 class SymbolTable
 {
-    private:
+    public: 
         struct SymNode {
             //Name of the node
             string name;
@@ -13,15 +16,49 @@ class SymbolTable
             //
             vector<SymNode *> children;
         }
-    public:
-        SymbolTable(void);
+        SymNode *symRoot;
+        SymNode *cur;
+
+        SymbolTable(string scopeName);
+        
+        /*
+         *Make a new scope (let, method, class) in the current scope
+         */ 
+        void addScope(string newScope);
+        
+        /*
+         * Enter a scope, making sure it exists first
+         */
+        void enterScope(string scope);
+        
+        /*
+         * Make a new scope in the current scope and enter it
+         */
+        void addAndEnterScope(string scope);
+
+        /*
+         * Get the name of the current scope
+         */
+        string getScope();
+
+        /*
+         *
+         */
+        void leaveScope();
+
+        void addVariable(string name, string type);
+
+        void addMethod(string name, vector<string> argTypes, string returnType);
+
+        SymTableVariable *getVariable(string name);
+        SymTableMethod *getMethod(string name);
+
 }
 
 class SymTableVariable 
 {
     //Type
     string type;
-    //Value??
 }
 
 class SymTableMethod
